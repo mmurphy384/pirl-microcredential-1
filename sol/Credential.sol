072266980c0bb522e5a1c495b98b40aee01fafdc
Michael Murphy <mmurphy384@yahoo.com>
2018-03-26 19:50:24 -0400

    More Changes

--- sol/Credential.sol
+++ sol/Credential.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.19;
 
 import "./Subscriber.sol";
+import "./MyUtils.sol";
 
-contract Credential {
+contract Credential is Subscriber, MyUtils {
 
     // Events
     event NewCredential(uint userId, uint credentialId, string credentialTitle);
@@ -39,10 +40,10 @@ contract Credential {
  
     // Public functions
     function AddCredential(string _subscriberPk, string _title,uint _epochTime, string _payload, string _credentialCode) public returns (bool) {
-        uint subscriberId = subscriberMapping[msg.sender];
+        uint orgId = orgMapping[msg.sender];
         uint id = 0;
-        if (subscriberId > 0) {
-            uint  key = _getCredentialKey(subscriberId,_subscriberPk);
+        if (orgId > 0) {
+            uint  key = _getCredentialKey(orgId,_subscriberPk);
             id = credentials.push(Credential(key, _title,_epochTime,_payload,_credentialCode,"pending"))-1;
         }
         return (id>0);
@@ -50,39 +51,39 @@ contract Credential {
     
     function AddFile(string _subscriberPk, string _fileName,string _fileFrindlyName, string _fileExtension) public returns (bool) {
         // TO DO:  Receive the incoming bytes and actually save the file to the masternode
-        uint subscriberId = subscriberMapping[msg.sender];
+        uint orgId = orgMapping[msg.sender];
         uint id = 0;
-        if (subscriberId > 0) {
-            uint key = _getCredentialKey(subscriberId,_subscriberPk);
+        if (orgId > 0) {
+            uint key = _getCredentialKey(orgId,_subscriberPk);
             credentialFileCount[key]++;
-            string memory storageFileName = _strConcat(_uint2str(key),"-",_uint2str(credentialFileCount[key]),_fileExtension,"");
-            id = files.push(File(_uint2str(key), _fileName,_fileFrindlyName,_fileExtension,storageFileName))-1;
+            string memory storageFileName = strConcat(uint2str(key),"-",uint2str(credentialFileCount[key]),_fileExtension,"");
+            id = files.push(File(uint2str(key), _fileName,_fileFrindlyName,_fileExtension,storageFileName))-1;
         }
         return (id>0);
     }     
  
     function Approve(string _subscriberPk, string _message) public returns (bool) {
-        uint subscriberId = subscriberMapping[msg.sender];
-        if (subscriberId > 0) {
-            uint key = _getCredentialKey(subscriberId,_subscriberPk);
+        uint orgId = orgMapping[msg.sender];
+        if (orgId > 0) {
+            uint key = _getCredentialKey(orgId,_subscriberPk);
             // ugh.  Loop through the array to find the credential and update the status/message
         }
         return (true);
     }   
 
     function Deny(string _subscriberPk, string _message) public returns (bool) {
-        uint subscriberId = subscriberMapping[msg.sender];
+        uint orgId = orgMapping[msg.sender];
         uint id = 0;
-        if (subscriberId > 0) {
-            uint key = _getCredentialKey(subscriberId,_subscriberPk);
+        if (orgId > 0) {
+            uint key = _getCredentialKey(orgId,_subscriberPk);
             // ugh.  Loop through the array to find the credential and update the status/message
         }
         return (true);
     }  
  
     // Private Functions
-    function _getCredentialKey(uint _subscriberId, string _subscriberPk) private pure returns (uint) {
-        string memory key =  strConcat(_uint2str(_subscriberId),"-", _subscriberPk,"","");
+    function _getCredentialKey(uint _orgId, string _subscriberPk) private pure returns (uint) {
+        string memory key =  strConcat(uint2str(_orgId),"-", _subscriberPk,"","");
         return uint(keccak256(key));
     } 
     
