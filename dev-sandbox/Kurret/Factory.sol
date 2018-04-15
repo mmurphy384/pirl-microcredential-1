a0b28d320f1a90fa40113c81c04853dfe393d439
jonlindg <ejonathanlindgren@gmail.com>
2018-04-15 15:03:16 +0200

    Factory contract, comments

--- dev-sandbox/Kurret/Factory.sol
+++ dev-sandbox/Kurret/Factory.sol
@@ -19,13 +19,16 @@ contract App  {
         _;
     }
 
+
+
+    //the type of credential, that the agency can define
     struct Credential {
         bytes32 requirements;
         bytes32 name;
         bool active;
     }
 
-
+    //application for approving a credential, from an application and then approved by agency
     struct CredentialApplication {
         bytes32 requirements;
         bytes32 name;
@@ -40,37 +43,40 @@ contract App  {
 
     Credential[] credentialList;
 
-  CredentialApplication[] credentials;
+    CredentialApplication[] credentials;
 
-  event CredentialSubmitted(address sender, uint credentialNumber);
-  event CredentialReceived(address sender, uint credentialNumber);
+    event CredentialSubmitted(address sender, uint credentialNumber);
+    event CredentialReceived(address sender, uint credentialNumber);
 
-  function App(address _owner) public {
-	owner = _owner;
-    creator = msg.sender;
-	}
+    function App(address _owner) public {
+        owner = _owner;
+        creator = msg.sender;
+    }
 
-  function getOwner() constant public returns (address){
-    return owner;
-  }
+    function getOwner() constant public returns (address){
+        return owner;
+    }
 
-  function getCreator() constant public returns (address){
-    return creator;
-  }
+    function getCreator() constant public returns (address){
+        return creator;
+    }
 
-  function submitMicroCredential(uint cred) public returns (bool){
-    require(cred<credentialList.length);
-    require(credentialList[cred].active==true);
-    //submitting a credential should include some documentation/photos, right?
-    //this could be a great usecase for the Pirl masternode system, namely the masternodes 
-    //will store the files for a period of time, at least until the agency has fetched the data themselves
-    //after the data has been fetched from the (storage) masternodes, the owner should perhaps call a function
-    //called "credentialReceived", acknowledging that the credential application has been seen, data has been received
-    //and the process for approval is submitted
 
-    CredentialSubmitted(msg.sender,credentials.length-1);
-  }
+    //submit a micro credential for approval by agency
+    function submitMicroCredential(uint cred) public returns (bool){
+        require(cred<credentialList.length);
+        require(credentialList[cred].active==true);
+        //submitting a credential should include some documentation/photos, right?
+        //this could be a great usecase for the Pirl masternode system, namely the masternodes 
+        //will store the files for a period of time, at least until the agency has fetched the data themselves
+        //after the data has been fetched from the (storage) masternodes, the owner should perhaps call a function
+        //called "credentialReceived", acknowledging that the credential application has been seen, data has been received
+        //and the process for approval is submitted
 
+        CredentialSubmitted(msg.sender,credentials.length-1);
+    }
+
+    //the agency creates a type of credential
     function createCredential(bytes32 requirements, bytes32 name) public onlyOwner returns (bool){
         Credential memory cred = Credential(requirements,name,true);
         credentialList.push(cred);
@@ -78,32 +84,37 @@ contract App  {
 
     }
 
-
+    //option to disable a credential
     function disableCredential(uint cred) public onlyOwner returns (bool){
         require(cred<credentialList.length);
         credentialList[cred].active=false;
     }
 
+    //option to enable a credential
     function enableCredential(uint cred) public onlyOwner returns (bool){
         require(cred<credentialList.length);
         credentialList[cred].active=true;
     }
 
-  function credentialReceived(uint index) public onlyOwner returns (bool){
-
-  }
-
-
-  function approveCredential(uint index) onlyOwner returns (bool) {
-    require(index<credentials.length);
-    require(!credentials[index].approved);
-    credentials[index].approved=true;
-    credentials[index].blockApproved=block.number;
-    credentials[index].timeApproved=block.timestamp;
+    //should be called by the agency when they have received the credential
+    //this function could be necessary if the supporting files are stored on storage masternodes
+    //the agency then calls this function when they have downloaded the files
+    function credentialReceived(uint index) public onlyOwner returns (bool){
+        //some code
+    }
 
 
-    return true;
-  }
+    //function to approve a submitted credential
+    function approveCredential(uint index) onlyOwner returns (bool) {
+        require(index<credentials.length);
+        require(!credentials[index].approved);
+        credentials[index].approved=true;
+        credentials[index].blockApproved=block.number;
+        credentials[index].timeApproved=block.timestamp;
+
+
+        return true;
+    }
 
 }
 
@@ -111,20 +122,20 @@ contract App  {
 contract Factory {
 
 
-mapping (address=>address) apps;
+    mapping (address=>address) apps;
 
-function deployNewContract() public returns (bool){
-require(apps[msg.sender]==address(0));
-address a;
-App app = new App(msg.sender);
-apps[msg.sender]=address(app);
-return true;
-}
+    function deployNewContract() public returns (bool){
+        require(apps[msg.sender]==address(0));
+        address a;
+        App app = new App(msg.sender);
+        apps[msg.sender]=address(app);
+        return true;
+    }
 
 
-function getContractAddress() constant public returns (address) {
-return apps[msg.sender];
-}
+    function getContractAddress() constant public returns (address) {
+        return apps[msg.sender];
+    }
 
 
 
